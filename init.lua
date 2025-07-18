5975646a6d0a79b7c89169a982d2e3eeddcebf7a
tenplus1 <kinsellaja@yahoo.com>
2025-07-18 17:50:36 +0100

    implement safety count to stop unlimited falling objects that crash system

--- init.lua
+++ init.lua
@@ -583,6 +583,77 @@ core.register_entity(":__builtin:falling_node", {
 })
 
 
+-- This table is specifically ordered.
+-- We don't walk diagonals, only our direct neighbors, and self.
+-- Down first as likely case, but always before self. The same with sides.
+-- Up must come last, so that things above self will also fall all at once.
+local check_for_falling_neighbors = {
+	vector.new(-1, -1,  0),
+	vector.new( 1, -1,  0),
+	vector.new( 0, -1, -1),
+	vector.new( 0, -1,  1),
+	vector.new( 0, -1,  0),
+	vector.new(-1,  0,  0),
+	vector.new( 1,  0,  0),
+	vector.new( 0,  0,  1),
+	vector.new( 0,  0, -1),
+	vector.new( 0,  0,  0),
+	vector.new( 0,  1,  0)
+}
+
+function core.check_for_falling(p)
+	-- Round p to prevent falling entities to get stuck.
+	p = vector.round(p)
+
+	-- We make a stack, and manually maintain size for performance.
+	-- Stored in the stack, we will maintain tables with pos, and
+	-- last neighbor visited. This way, when we get back to each
+	-- node, we know which directions we have already walked, and
+	-- which direction is the next to walk.
+	local s = {}
+	local n = 0
+	local v = 1 -- first neighbor to visit from ourtable.
+
+	while true do
+		-- Push current pos onto the stack.
+		n = n + 1 ; if n > 1000 then return end -- FIX: safety count to stop crash!
+		s[n] = {p = p, v = v}
+		-- Select next node from neighbor list.
+		p = vector.add(p, check_for_falling_neighbors[v])
+		-- Now we check out the node. If it is in need of an update,
+		-- it will let us know in the return value (true = updated).
+
+		if not core.check_single_for_falling(p) then
+			-- If we don't need to "recurse" (walk) to it then pop
+			-- our previous pos off the stack and continue from there,
+			-- with the v value we were at when we last were at that
+			-- node
+
+			repeat
+				local pop = s[n]
+				p = pop.p
+				v = pop.v
+				s[n] = nil
+				n = n - 1
+				-- If there's nothing left on the stack, and no
+				-- more sides to walk to, we're done and can exit
+				if n == 0 and v == 11 then
+					return
+				end
+			until v < 11
+
+			-- The next round walk the next neighbor in list.
+			v = v + 1
+		else
+			-- If we did need to walk the neighbor, then
+			-- start walking it from the walk order start (1),
+			-- and not the order we just pushed up the stack.
+			v = 1
+		end
+	end
+end
+
+
 --[[
 core.override_item("default:gravel", {
 	falling_step = function(self, pos, dtime)
