403873438b558a53f99222a2083764a3e672aecf
tenplus1 <kinsellaja@yahoo.com>
2026-05-20 16:26:49 +0100

    as simple as it gets, refactor check_for_falling (again)

--- init.lua
+++ init.lua
@@ -550,123 +550,40 @@ core.register_entity(":__builtin:falling_node", {
 	end
 })
 
---[[
--- This table is specifically ordered.
--- We don't walk diagonals, only our direct neighbors, and self.
--- Down first as likely case, but always before self. The same with sides.
--- Up must come last, so that things above self will also fall all at once.
-local check_for_falling_neighbors = {
-	vector.new(-1, -1,  0), vector.new( 1, -1,  0), vector.new( 0, -1, -1),
-	vector.new( 0, -1,  1), vector.new( 0, -1,  0), vector.new(-1,  0,  0),
-	vector.new( 1,  0,  0), vector.new( 0,  0,  1), vector.new( 0,  0, -1),
-	vector.new( 0,  0,  0), vector.new( 0,  1,  0)
-}
-
-function core.check_for_falling(p)
-
-	-- Round p to prevent falling entities to get stuck.
-	p = vector.round(p)
-
-	-- We make a stack, and manually maintain size for performance.
-	-- Stored in the stack, we will maintain tables with pos, and
-	-- last neighbor visited. This way, when we get back to each
-	-- node, we know which directions we have already walked, and
-	-- which direction is the next to walk.
-	local s = {}
-	local n = 0
-	local v = 1 -- first neighbor to visit from ourtable.
-
-	while true do
-		-- Push current pos onto the stack.
-		n = n + 1
-		s[n] = {p = p, v = v}
-
-		-- FIX: limit falling nodes stop crash!
-		if n > 1500 then return end
-
-		-- Select next node from neighbor list.
-		p = vector.add(p, check_for_falling_neighbors[v])
-		-- Now we check out the node. If it is in need of an update,
-		-- it will let us know in the return value (true = updated).
-
-		if not core.check_single_for_falling(p) then
-			-- If we don't need to "recurse" (walk) to it then pop
-			-- our previous pos off the stack and continue from there,
-			-- with the v value we were at when we last were at that
-			-- node
-
-			repeat
-				local pop = s[n]
-				p = pop.p
-				v = pop.v
-				s[n] = nil
-				n = n - 1
-				-- If there's nothing left on the stack, and no
-				-- more sides to walk to, we're done and can exit
-				if n == 0 and v == 11 then return end
-			until v < 11
-
-			-- The next round walk the next neighbor in list.
-			v = v + 1
-		else
-			-- If we did need to walk the neighbor, then
-			-- start walking it from the walk order start (1),
-			-- and not the order we just pushed up the stack.
-			v = 1
-		end
-	end
-end
-]]
-
 local falling_neighbors = {
 	vector.new(-1, -1,  0), vector.new( 1, -1,  0), vector.new( 0, -1, -1),
 	vector.new( 0, -1,  1), vector.new( 0, -1,  0), vector.new(-1,  0,  0),
 	vector.new( 1,  0,  0), vector.new( 0,  0,  1), vector.new( 0,  0, -1),
 	vector.new( 0,  0,  0), vector.new( 0,  1,  0)
 }
-local fn_count = #falling_neighbors
-local stack_depth = 1000 -- limits falling nodes to avoid crashing
 
 function core.check_for_falling(p)
 
-	p = vector.round(p)
+	local stack = {vector.round(p)}
+	local count = 0
+	local max_depth = 650
 
-	local stack = {}
-	local size = 0
+	while #stack > 0 and count < max_depth do
 
-	local function push_node(pos)
+		local current_pos = table.remove(stack) -- store and remove position
 
-		size = size + 1
+		count = count + 1
 
-		if size > stack_depth then return end
+		for _, offset in ipairs(falling_neighbors) do
 
-		stack[size] = {p = pos, v = 1}
-
-		return true
-	end
-
-	push_node(p) -- first prod
-
-	while size > 0 do
-
-		local current = stack[size]
-
-		if current.v > fn_count then
-			stack[size] = nil
-			size = size - 1
-		else
-			local next_pos = vector.add(current.p, falling_neighbors[current.v])
-
-			current.v = current.v + 1
-
+			local next_pos = vector.add(current_pos, offset)
+			
+			-- If node be falling, add to stack for a neighbor check
 			if core.check_single_for_falling(next_pos) then
 
-				if not push_node(next_pos) then return end
+				table.insert(stack, next_pos)
+				
+				-- Incase of massive collapses
+				if #stack + count > max_depth then return end
 			end
 		end
 	end
 end
-
 
 --[[
 core.override_item("default:gravel", {
